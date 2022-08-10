9b7e68220f2cd77bfbe4d371aff9f499a9b1472e
Zeynep Kucur <zeynepkucur@gmail.com>
2022-08-10 14:23:20 +0200

    header changed for mobile

--- Js.js
+++ Js.js
@@ -28,3 +28,33 @@ window.addEventListener("scroll", () => {
     toTop.classList.remove("active");
   }
 })
+
+const hamburger = document.getElementById('hamburger')
+const sidebar = document.getElementById('sidebarLinks')
+const overlay = document.getElementById('overlay')
+
+let menuOpen = false
+
+function openMenu() {
+  menuOpen = true
+  overlay.style.display = 'block'
+  sidebar.style.width = '250px'
+}
+
+function closeMenu() {
+  menuOpen = false
+  overlay.style.display = 'none'
+  sidebar.style.width = '0px'
+}
+
+hamburger.addEventListener('click', function () {
+  if (!menuOpen) {
+    openMenu()
+  }
+})
+
+overlay.addEventListener('click', function () {
+  if (menuOpen) {
+    closeMenu()
+  }
+})
